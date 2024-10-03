aba48faafc1442df6ff1a345f320b208c455182e
Kurogami2134 <90238011+Kurogami2134@users.noreply.github.com>
2024-10-03 14:08:20 -0300

    Fixed missing end for modloader.bin

--- src/build.py
+++ src/build.py
@@ -4,3 +4,4 @@ with open("bin/ml", "rb") as ml, open("bin/mlhooks", "rb") as hooks, open("bin/m
     data = ml.read()
     mod.write(pack("2I", 0x08800500, len(data)))
     mod.write(data)
+    mod.write(pack("2i", -1, 0))
